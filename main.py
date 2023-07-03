44bb6c14917c69e9218f2b19dcb01c76e8adb1e9
schender0day <schender0day@gmail.com>
2023-07-03 00:05:41 -0700

    more

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import requests
 import json
 import logging
 
+import os
+import requests
+from datetime import datetime
 logging.basicConfig(level=logging.INFO)
 
 # Load the API key from a local file
@@ -23,7 +26,8 @@ def get_robots():
         res = conn.getresponse()
         data = res.read()
         pretty_data = json.loads(data.decode("utf-8"))
-        print(json.dumps(pretty_data, indent=4))
+        return pretty_data
+        # print(json.dumps(pretty_data, indent=4))
     except Exception as e:
         logging.error(f'Failed to get robots: {e}')
 
@@ -36,7 +40,8 @@ def get_robot_tasks(robotId):
         res = conn.getresponse()
         data = res.read()
         pretty_data = json.loads(data.decode("utf-8"))
-        print(json.dumps(pretty_data, indent=4))
+        # print(json.dumps(pretty_data, indent=4))
+        return pretty_data
     except Exception as e:
         logging.error(f'Failed to get robot tasks: {e}')
 
@@ -49,6 +54,8 @@ def get_robot_tasks_page(robotId, pageNumber):
         res = conn.getresponse()
         data = res.read()
         pretty_data = json.loads(data.decode("utf-8"))
+        return pretty_data
+    except Exception as e:
         print(json.dumps(pretty_data, indent=4))
     except Exception as e:
         logging.error(f'Failed to get robot tasks page: {e}')
@@ -61,7 +68,8 @@ def post_robot_task(robotId, originUrl):
         headers = {"Authorization": f"Bearer {get_api_key()}"}
         response = requests.request("POST", url, json=payload, headers=headers)
         pretty_data = json.loads(response.text)
-        print(json.dumps(pretty_data, indent=4))
+        return pretty_data
+        # print(json.dumps(pretty_data, indent=4))
     except Exception as e:
         logging.error(f'Failed to post robot task: {e}')
 
@@ -77,3 +85,56 @@ def get_specific_task(robotId, taskId):
         print(json.dumps(pretty_data, indent=4))
     except Exception as e:
         logging.error(f'Failed to get specific task: {e}')
+
+def get_png_filenames(response):
+    png_files = []
+
+    try:
+        tasks = response['data']['browseai']['getRobot']['tasks']
+        for task in tasks:
+            screenshots = task['data']['capturedScreenshots']
+            for screenshot in screenshots.values():
+                src = screenshot['src']
+                if src.endswith('.png'):
+                    png_files.append(src)
+    except KeyError:
+        print('Unable to find key in the response.')
+
+    return png_files
+def download_screenshots(screen_shot_robot_id):
+    # Create the screenshots directory
+    current_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
+    directory = f"./screenshots/{current_time}"
+    os.makedirs(directory, exist_ok=True)
+
+    # Retrieve the robot tasks
+    res = get_robot_tasks(screen_shot_robot_id)
+
+    # Extract PNG filenames and download them
+    count = 1  # Counter for the filename
+    for item in res["result"]['robotTasks']['items'][-1]['capturedScreenshots'].values():
+        url = item['src']
+        extension = os.path.splitext(url)[1]
+        filename = f"{count}{extension}"
+        filepath = os.path.join(directory, filename)
+
+        # Download the PNG file
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(filepath, 'wb') as f:
+                f.write(response.content)
+                print(f"Downloaded: {filepath}")
+        else:
+            print(f"Failed to download: {url}")
+
+        count += 1  # Increment the counter
+
+
+
+deal_list_robot_id = "fa361dc9-4801-4c6c-8e46-907865508e05"
+screen_shot_robot_id = "da5987ee-0d04-4b70-96f9-6ae0fb5ec391"
+# download_screenshots(screen_shot_robot_id)
+res = get_robot_tasks(deal_list_robot_id)
+# print(res['result']['robotTasks']['items'][-1]['capturedLists']['amazon product list parser'])
+for item in res['result']['robotTasks']['items'][-1]['capturedLists']['amazon product list parser']:
+    print(item['Position'], item['discount rate'], item['product name'])
